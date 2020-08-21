e9c7b6dafd48a9d6c31a86539dd615959089dca3
Matteo Nigro <matteo95nigro@gmail.com>
2020-08-21 18:20:56 +0200

    Remodified the daily.js file with A LOT of blasphemy word because i lost hours of work...no words

--- commands/daily.js
+++ commands/daily.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const TeamManager = require('../TeamManager');
 const config = require('../config.json');
+const ReviewChannelManager = require('../ReviewChannelManager');
 
 module.exports = {
     name: 'daily',
@@ -37,19 +38,7 @@ module.exports = {
             webSocket.team = TeamManager.ReadJsonTESTING();
         }
 
-        var channels = [];
-        webSocket.client.guilds.cache.first().channels.cache
-            .filter(c => c.type == 'text' && c.name == config.defaultChannel)
-            .forEach(c => {
-                channels.push(c.id);
-            });
-
-        if (!channels)
-            return message.author.send(`Non sono presenti canali testuali con il nome ${config.defaultChannel}! Sistema o, se non puoi, contatta subito chi ha i permessi per farlo`);
-
-        if (channels.length > 1)
-            return message.author.send(`Ci sono multipli canali testuali con il nome ${config.defaultChannel}! Sistema o, se non puoi, contatta subito chi ha i permessi per farlo`);
-
+        const channels = ReviewChannelManager.GetReviewChannelID(webSocket, message);
         webSocket.reviewChannelID = channels.shift();
 
 
